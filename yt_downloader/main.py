fb35e4cdb90abbb5f71e9feaaf8aac52609e1fd2
Luan <luanmiguel.contato@outlook.com>
2025-08-19 17:41:46 -0300

    finished
    baixa o video 720p com audio

--- yt_downloader/main.py
+++ yt_downloader/main.py
@@ -1,21 +1,34 @@
 from pytubefix import YouTube
 from pytube.cli import on_progress
+import subprocess
+import os
 
-url = 'https://www.youtube.com/watch?v=NpU_aN54eJA&list=RDNpU_aN54eJA&start_radio=1'
+url = str(input('Digite a url:'))
 destino = 'yt_downloader/pasta_video'
 
 yt = YouTube(url, on_progress_callback=on_progress)
 print(yt.title)
 
-# Tenta pegar 720p progressive (com áudio)
-ys = yt.streams.filter(res="720p", progressive=True).first()
+# Baixa vídeo 720p (DASH, sem áudio)
+video_stream = yt.streams.filter(res="720p", type="video").first()
+# Baixa áudio separado
+audio_stream = yt.streams.filter(only_audio=True).first()
 
-# Se não tiver progressive 720p, pega 720p DASH (vídeo sem áudio)
-if not ys:
-    ys = yt.streams.filter(res="720p", type="video").first()
+video_path = os.path.join(destino, "video.mp4")
+audio_path = os.path.join(destino, "audio.mp4")
+final_path = os.path.join(destino, f"{yt.title}.mp4")
 
-if ys:
-    ys.download(output_path=destino)
-    print("Download concluído!")
-else:
-    print("Vídeo em 720p não disponível.")
+video_stream.download(output_path=destino, filename="video.mp4")
+audio_stream.download(output_path=destino, filename="audio.mp4")
+
+# Junta vídeo e áudio com ffmpeg
+subprocess.run([
+    "ffmpeg", "-y", "-i", video_path, "-i", audio_path,
+    "-c:v", "copy", "-c:a", "aac", final_path
+])
+
+# Remove arquivos temporários
+os.remove(video_path)
+os.remove(audio_path)
+
+print("Download 720p com áudio concluído!")
