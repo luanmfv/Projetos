ad60b75adf23bec9938b6c23d51dc41e433f77c6
Luan <luanmiguel.contato@outlook.com>
2025-10-06 14:54:32 -0300

    changes

--- fullstack/polls/views.py
+++ fullstack/polls/views.py
@@ -4,6 +4,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Olá Luan, aqui é a pagina polls que você inseriu =D")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("Você está olhando para a questão %s." % question_id)
+
+def results(request, question_id):
+    response = "Você está olhando para os resultados da questão %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão %s." % question_id)
